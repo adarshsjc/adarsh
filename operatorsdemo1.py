3f84e4582babf10a7067b693969654d50ff4ecad
adarshsjc <av200489adarshvish.vg@gmail.com>
2024-10-18 16:00:26 +0530

    Add files via upload

---
+++ operatorsdemo1.py
@@ -0,0 +1,14 @@
+'''
+Auther:Adarsh v
+Date:18/10/24
+Description: a Python program that demonstrates the usage of arithmetic, comparison, and logical operators.
+Perform a few operations and print the results.
+'''
+
+number1=int (input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print("sum=",number1+number2,",Division=",number1/number2)
+print("Is number1 greater than number2?: ",number1>number2)
+print("Are number1 and number2 equal?:",number1==number2)
+print("Logical AND:",number1>0 and number2>0)
+print("Logical OR :",number1>0 or number2>number1)

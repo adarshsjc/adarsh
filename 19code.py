9b90bc663c2e7a162d101fe38bf7e2472d706c83
adarshsjc <av200489adarshvish.vg@gmail.com>
2024-10-19 14:52:39 +0530

    Add files via upload

---
+++ 19code.py
@@ -0,0 +1,33 @@
+'''
+Author:Adarsh v
+Date:19/10/24
+Description:Python program that performs the following tasks:
+Addition,subtraction,multiplication,division,modulus,combined arithemetic operations
+
+'''
+
+
+
+
+
+num1=int(input("Enter num1:"))
+num2=int(input("Enter num2:"))
+num3=int(input("Enter num3:"))
+
+#Addition
+print("The sum of num1 and num2 is:",num1+num2)
+
+#Subtrction
+print("The difference when num2 is subtracted from num1 is:",num1-num2)
+
+#Multiplication
+print("The product of num1 and num2 is:",num1*num2)
+
+#Divison
+print("The quotient when num1 is divided by num2:",num1/num2)
+
+#Modulus
+print("The reminder when num1 is divided by num2 is:",num1%num2)
+
+#combined Arithemetic operation
+print("The result of (num1 + num2) * num3 / 2 is:",(num1 + num2) * num3 /2 )

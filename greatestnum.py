eed2955ce5509d5025500a6fb392e0480ac6c9bf
adarshsjc <av200489adarshvish.vg@gmail.com>
2024-10-25 16:14:15 +0530

    Update greatestnum.py

--- greatestnum.py
+++ greatestnum.py
@@ -1,3 +1,12 @@
+'''
+Author:Adarsh v
+Date:25/10/24
+Description:Write a Python program to find the largest of three numbers.
+ The program should take three numbers as input from the user and determine which one is the largest.
+Use conditional statements to compare the numbers.
+'''
+
+
 number1=int(input("Enter the first number:"))
 number2=int(input("Enter the second number:"))
 number3=int(input("Enter the third number:"))

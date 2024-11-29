ce42c0f2007af52c0e171dee1ab6bb7bf48b6d42
adarshsjc <av200489adarshvish.vg@gmail.com>
2024-11-29 15:21:29 +0530

    Add files via upload

---
+++ triangle_function.py
@@ -0,0 +1,22 @@
+def triangle(a,b,c):
+    if  a**2 ==c**2 + b**2:
+        print("Right angle triangle")
+    else :
+        print("Not right angle triangle")
+
+
+
+
+g=int(input("Enter the hypotenuse side"))
+h=int(input("Enter the second side"))
+i=int(input("Enter the third side"))
+triangle(g,h,i)
+
+
+
+
+
+
+
+
+
